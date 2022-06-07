721137ee7388a78ff2a377e0379ac91342a752fe
Elias Mansfield <eliasmansfield@MacBook-Air-tilhrende-Elias.local>
2022-06-08 01:48:23 +0200

    All functions done with console interface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -52,7 +54,6 @@ def best_of_24_hours():
     print("")
 
     driver.quit()
-
 
 def best_of_12_hours():
     driver = webdriver.Chrome()
@@ -177,6 +178,88 @@ def best_under_x():
 
     driver.quit()
 
+def see_all():
+    driver = webdriver.Chrome()
+    driver.get(web)
+    driver.minimize_window()
+
+    try:
+        wait = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "graph-bar__tooltip"))
+        )
+    except:
+        driver.quit()
+        print("Prøv igen!")
+
+    list = []
+    kl = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
+          "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+
+    for x in range(0, 24):
+        data = driver.find_element(By.CLASS_NAME, 'graph-bar__tooltip').get_attribute("textContent")
+        data = data.replace("øre/kWh", "")
+        data = data.replace("\n    ", "")
+        data = data.replace(",", ".")
+        list.append(float(data))
+        data = driver.find_element(By.CLASS_NAME, 'graph-bar__tooltip')
+        driver.execute_script("""
+                var element = arguments[0];
+                element.parentNode.removeChild(element);
+                """, data)
+
+    print("")
+    print("################################################")
+    for x in range(0, 24):
+        try:
+            final = str(round(list[x] / 100, 2)).replace(".", ",")
+            print(kl[x] + " med " + final + "kr")
+        except:
+            pass
+    print("################################################")
+    print("")
+
+    driver.quit()
+
+def see_mid():
+    driver = webdriver.Chrome()
+    driver.get(web)
+    driver.minimize_window()
+
+    try:
+        wait = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "graph-bar__tooltip"))
+        )
+    except:
+        driver.quit()
+        print("Prøv igen!")
+
+    list = []
+    kl = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
+          "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+
+    for x in range(0, 24):
+        data = driver.find_element(By.CLASS_NAME, 'graph-bar__tooltip').get_attribute("textContent")
+        data = data.replace("øre/kWh", "")
+        data = data.replace("\n    ", "")
+        data = data.replace(",", ".")
+        list.append(float(data))
+        data = driver.find_element(By.CLASS_NAME, 'graph-bar__tooltip')
+        driver.execute_script("""
+                var element = arguments[0];
+                element.parentNode.removeChild(element);
+                """, data)
+
+    sum = math.fsum(list)/100
+    sum = round(sum / 24, 2)
+
+    print("")
+    print("################################################")
+    print("Gennemsnittet for idag bliver " + str(sum) + "kr")
+    print("################################################")
+    print("")
+
+    driver.quit()
+
 def menu():
     print("")
     print(" EL priser i Odense")
@@ -205,10 +288,13 @@ def main_functions():
         elif PEIN == "3":
             best_under_x()
             break
+        elif PEIN == "4":
+            see_all()
+            break
         else:
             print("")
             print("Det er ikke en valgmulighed!")
             print("")
 
 
-main_functions()
+see_mid()
